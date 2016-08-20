96cba1a815416dfaf66792459fb91839219ba82c
rg3915 <rg3915@yahoo.com.br>
2016-08-20 16:57:49 -0300

    core app

---
+++ eventex/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'', include('eventex.core.urls', namespace='core')),
+    # url(r'^inscricao/', include('eventex.subscriptions.urls', namespace='subscriptions')),
+    url(r'^admin/', admin.site.urls),
+]
